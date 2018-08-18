1ec7fd509b3d3b3c0e245ef5d386e7af9905f58f
kooldrmony <derrickreyes.23@gmail.com>
2018-08-18 15:57:14 -0700

    added messagelist component update

--- chatter/src/App.js
+++ chatter/src/App.js
@@ -1,18 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import MessageList from './components/MessageList'
+import Chatkit from '@pusher/chatkit';
+import { instanceLocator, tokenUrl } from './components/config'
 
 class App extends Component {
+
+  componentDidMount() {
+
+    const chatManager = new Chatkit.ChatManager({
+      instanceLocator,
+      userId: 'Derrick',
+      tokenProvider: new Chatkit.TokenProvider({
+        url: tokenUrl
+      })
+    })
+
+    chatManager.connect()
+    .then(currentUser => {
+      currentUser.subscribeToRoom({
+        roomId: 13776982,
+        hooks: {
+          onNewMessage: message => {
+            console.log('message.text ', message.text);
+          }
+        }
+      })
+    })
+  }
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <MessageList />
+   
       </div>
     );
   }
